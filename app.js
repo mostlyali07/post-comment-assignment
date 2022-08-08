3d3d40b2ec061a969d1155b226ceed19abbbe09d
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-08-08 17:09:49 +0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var parentDiv = document.getElementById("div1");
-var newParagraph = document.createElement("p");
-var t = document.createTextNode("Hello world!");
-newParagraph.appendChild(t);
-paragraph1 = parentDiv.firstChild;
-parentDiv.insertBefore(newParagraph, paragraph1);
+var post = document.getElementById("post");
+post.addEventListener("click", function () {
+    var commentBoxValue = document.getElementById("comment-box").value;
+
+    var h3 = document.createElement("h3");
+    var text = document.createTextNode(commentBoxValue);
+    h3.appendChild(text);
+    document.getElementById("unordered").appendChild(h3);
+    event.preventDefault();
+});
